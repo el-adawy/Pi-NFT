8954e710401ae849d1745024df2044ea6e1d5c79
el adawy <ahmed-mohamed@outlook.com>
2021-09-05 01:20:10 +0200

    init_php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 try{
     $pdo = new PDO('sqlite:'.dirname(__FILE__).'/database.sqlite');
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
