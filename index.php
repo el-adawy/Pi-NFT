25bbad992a9d156dc02ce3e399ca6dd29336ed8f
el adawy <ahmed-mohamed@outlook.com>
2021-09-05 01:28:35 +0200

    init_php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 try{
-    $pdo = new PDO('sqlite:'.dirname(__FILE__).'/database.sqlite');
+    $pdo = new PDO('sqlite:'.dirname(__FILE__).'/nft.db');
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
 } catch(Exception $e) {
