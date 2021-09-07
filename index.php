7f4c540a4d06b2b6b879aab8737237a7aa12a554
el adawy <ahmed-mohamed@outlook.com>
2021-09-07 23:17:13 +0200

    commit

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ try{
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="#">Start Bootstrap</a>
+          <a class="navbar-brand" href="#">Uneat</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -46,9 +46,6 @@ try{
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
