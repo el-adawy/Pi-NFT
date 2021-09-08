6b3c70768072ccc82c8634f3ee02e83a644e6eae
el adawy <ahmed-mohamed@outlook.com>
2021-09-08 19:02:08 +0200

    commit

--- index.php
+++ index.php
@@ -11,33 +11,29 @@ try{
     echo "Impossible d'accéder à la base de données SQLite : ".$e->getMessage();
     die();
 }
+
 ?>
 
-<html lang="en"><head>
+<html lang="en">
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-
     <title>Pi Network - NFT</title>
-
-
     <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="album.css" rel="stylesheet">
   </head>
 
   <body>
-
-
       <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
           <a class="navbar-brand" href="#">Uneat</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
@@ -55,112 +51,86 @@ try{
         </div>
       </nav>
 
-    <header>
-     
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-    </header>
-
     <main role="main">
 
       <section class="jumbotron text-center">
         <div class="container">
           <h1 class="jumbotron-heading"></h1>
           <p class="lead text-muted">
-NFTs are a fun and smart way to deal with cryptocurrencies.
-Here at uneat APP you can Buy awesome NFTs, sell your own and take chances bidding public NFTs.
-
-</p>
+            NFTs are a fun and smart way to deal with cryptocurrencies.
+            Here at uneat APP you can Buy awesome NFTs, sell your own and take chances bidding public NFTs.
+          </p>
           <p>
-
-
-You can also enter special events to get special edition Pi NFTs, breeding NFT from featured artists or even get breeding NFTs. Are you curious? Welcome to uneat NFT world.
+            You can also enter special events to get special edition Pi NFTs, breeding NFT from featured artists or even get breeding NFTs. Are you curious? Welcome to uneat NFT world.
           </p>
         </div>
       </section>
 
-      <div class="album py-5 bg-light"
-">
+      <div class="album py-5 bg-light">
         <div class="container">
 
 <?php
 
+
+  function display_nft_card($id, $title, $creator_name, $description, $price){
+
+    echo '<div class="col-md-4">
+    <div class="card mb-4 box-shadow">	       
+      <a href="index.php?nft-id=' . $id .'"><img class="card-img-top" src="/img/nfts/NFT_' . $id . '.jpg" data-holder-rendered="true" style="height: 300px"></a>
+      <div class="card-body">
+        <p class="card-text">' . $title . ' - @' . $creator_name . '</p>
+        <p class="card-text">You can Buy now this NFT or wait until you can bid on a Auction.</p>
+        <div class="btn-group">
+          <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Bid</button>
+        </div>
+        <div class="d-flex justify-content-between align-items-center"><br>
+          <small class="text-muted"><img src="/img/pi.png" style="height: 10px; margin-bottom: 3px">  '. round($price, 3) . ' </small>
+        </div>
+      </div>
+    </div>
+</div>';
+
+  }
+
 	if (!isset($_GET['nft-id'])){
+        $query = $pdo->query('SELECT * FROM NFT');
+        echo '<div class="row">';
+        $i = 1;
+        while ($data = $query->fetch()) {
+        $i++;
 
-	  $query = $pdo->query('SELECT * FROM NFT');
+        display_nft_card($data['ID'], $data['TITLE'], $data['CREATOR_NAME'], $data['DESCRIPTION'], $data['PRICE']);
 
-
-	  echo '<div class="row">';
-
-	  $i = 1;
-
-	  while ($data = $query->fetch()) {
-	  $i++;
-
-
-	  echo '<div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-	       
-                <a href="index.php?nft-id=' . $data['ID'] .'"><img class="card-img-top" src="/img/nfts/NFT_' . $data['ID'] . '.jpg" data-holder-rendered="true" style="height: 300px"></a>
-
-		<div class="card-body">
-                  <p class="card-text">' . $data['TITLE'] . ' - @' . $data['CREATOR_NAME'] . '</p>
-
-              <p class="card-text">You can Buy now this NFT or wait until you can bid on a Auction.</p>
-
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Bid</button>
-                </div>
-
-              <div class="d-flex justify-content-between align-items-center"><br>
-                <small class="text-muted"><img src="/img/pi.png" style="height: 10px; margin-bottom: 3px">  '. round($data['PRICE'], 3) . ' </small>
-              </div>
-            </div>
-	    </div>
-	    </div>
-              ';
-	  }
+	}
          
-	 echo'      </div>
-	       </div> ';
+	 echo'</div></div>';
+	}//endIf
 
-	       }//endIf
+	else {//click on a nft	       
+        $query = $pdo->query('SELECT * FROM NFT WHERE ID = :id_get');
+        $data = $query->execute(array('id_get' => htmlspecialchars($_GET['nft-id'])));
+        $data = $query->fetch();            
+            echo '<div class="row ">            
+            <div class="col-md-4">
+                  <div class="card mb-4 box-shadow">            
+                    <a href="index.php?nft-id=' . $data['ID'] .'"><img class="card-img-top" src="/img/nfts/NFT_' . $data['ID'] . '.jpg" data-holder-rendered="true" style="height: 300px"></a>
+                  <div class="card-body">
+                      <p class="card-text">' . $data['TITLE'] . ' - @' . $data['CREATOR_NAME'] . '</p>
 
-	       else {
-	       //click on a nft
-	       
-	  $query = $pdo->query('SELECT * FROM NFT WHERE ID = :id_get');
-	  $data = $query->execute(array('id_get' => htmlspecialchars($_GET['nft-id'])));
-	  $data = $query->fetch();
-	       
-	       echo '<div class="row ">
-	       
-	      <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-	       
-                <a href="index.php?nft-id=' . $data['ID'] .'"><img class="card-img-top" src="/img/nfts/NFT_' . $data['ID'] . '.jpg" data-holder-rendered="true" style="height: 300px"></a>
+                  <p class="card-text">' . $data['DESCRIPTION'] . '<br />You can Buy now this NFT or wait until you can bid on a Auction.</p>
 
-		<div class="card-body">
-                  <p class="card-text">' . $data['TITLE'] . ' - @' . $data['CREATOR_NAME'] . '</p>
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Bid</button>
+                    </div>
 
-              <p class="card-text">' . $data['DESCRIPTION'] . '<br />You can Buy now this NFT or wait until you can bid on a Auction.</p>
-
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Bid</button>
+                  <div class="d-flex justify-content-between align-items-center"><br>
+                    <small class="text-muted"><img src="/img/pi.png" style="height: 10px; margin-bottom: 3px">  '. round($data['PRICE'], 3) . ' </small>
+                  </div>
                 </div>
-
-              <div class="d-flex justify-content-between align-items-center"><br>
-                <small class="text-muted"><img src="/img/pi.png" style="height: 10px; margin-bottom: 3px">  '. round($data['PRICE'], 3) . ' </small>
-              </div>
-            </div>
-	    </div>
-	    </div>';
+          </div>
+          </div>';
 
 
 	       }
